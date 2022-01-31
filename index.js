69714a3930425bb3982cdb8b4628fde7c8aa3388
Emil Məmmədov <emill.memmedovv@gmail.com>
2022-01-31 15:13:12 +0400

    init

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+require('dotenv/config')
+
+const drivers = ['AzeriCart'];
+let driverName = ''
+
 function driver(driver) {
-    return driver === 'AzeriCart'
+    if (!drivers.includes(driver)){
+        throw new Error('driver not found')
+    }
+    else{
+        driverName = driver;
+    }
 }
 
-module.exports = driver()
+function apply(data){
+    switch (driverName){
+        case "AzeriCart": {
+            applyForAzericart(data);
+        }
+    }
+}
+
+function applyForAzericart(data){
+    if (!process.env.AZERICARD_URL){
+        throw Error('please add Azeri card URL to .env file');
+    }
+    else{
+
+    }
+}
+
+module.exports = {
+    driver: driver,
+    apply: apply
+}
